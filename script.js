8dc30c5accd2fc62a129e0272e3584c0f34ddd9d
salim4246 <mdsalimhossain3192@gmail.com>
2025-01-30 14:07:50 +0100

    javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+const x=125
+const y=285
+result=x+y;
+console.log(result);
+
+womenshoes=80
+menshoes=79
+tex=1.90
+delivery=8
+discount=25
+
+console.log(womenshoes+menshoes+tex+delivery-discount);
+
+
+
+let rahimTotalmoney = 1500;
+let egg = 24;
+let kitkat = 12;
+let eachEgg = 15;
+let eachKitkat = 60;
+
+let eggPrice = egg*eachEgg;
+let kitkatPrice = kitkat*eachKitkat;
+
+let totalPrice = eggPrice+kitkatPrice;
+
+let remainingMoney = rahimTotalmoney-totalPrice;
+
+console.log(remainingMoney);
